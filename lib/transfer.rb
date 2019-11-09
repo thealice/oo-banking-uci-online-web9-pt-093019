57c73713999a49ab1a72609a64bab0a73c6c64d1
Alice Freda <alicefreda@gmail.com>
2019-11-09 03:53:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,17 @@
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
+
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
     @status = "pending"
+  end
+
+  def valid?
+    sender_acct = BankAccount.all.detect {|account| account.name == sender}.valid?
+    receiver_acct = BankAccount.all.detect {|account| account.name == receiver}.valid?
+    sender_acct && receiver_acct
+    end
   end
 end
