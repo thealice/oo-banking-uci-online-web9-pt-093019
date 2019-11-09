73df54dac41c97f7faad0c0b5741a22b9354b3a8
Alice Freda <alicefreda@gmail.com>
2019-11-09 03:32:13 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,14 @@
 class BankAccount
   attr_reader :name
   attr_accessor :balance, :status
+
   def initialize(name, balance=1000)
     @name = name
     @balance = 1000
     @status = "open"
+  end
+
+  def deposit(money)
+    self.balance += money
   end
 end
