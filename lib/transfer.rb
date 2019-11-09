d88d474c814ebbcda6a86889a3f46f9802cf051a
Alice Freda <alicefreda@gmail.com>
2019-11-09 04:03:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,7 +11,7 @@ class Transfer
 
   def valid?
         binding.pry
-    sender_acct = BankAccount.all.detect?(|account| account.name == sender.name})
+    sender_acct = BankAccount.all.detect?(|account| account.name == sender.name)
     receiver_acct = BankAccount.all.select {|account| account.name == receiver.name}.valid?
 
     sender_acct && receiver_acct
